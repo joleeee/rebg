b780b5f154e3371cea0e11bc206840ed0c879346
Jonas <jonas@jole.xyz>
2023-05-30 22:47:35 +0200

    print pc

--- rebg/src/rstate.rs
+++ rebg/src/rstate.rs
@@ -49,6 +49,9 @@ impl<const N: usize> StateDiff<N> {
         for (i, a, b) in &diff_regs {
             println!("{} <- {:x} (prev {:x})", S::reg_name(*i), b, a);
         }
+        if let RDiff::Changed { from: _, to } = self.pc {
+            println!("pc <- {:x}", to);
+        }
 
         // ignore pc
 
