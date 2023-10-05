643bfe535ccf9e84b297ffdd44f96672375e6117
Jonas <jonas@jole.xyz>
2023-10-06 00:48:45 +0200

    kinda working, shitty css tho

--- rebg-web/src/App.svelte
+++ rebg-web/src/App.svelte
@@ -1,49 +1,30 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  import Stepline from "./lib/Stepline.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
+<div class="timeline"><div class="padder">some list here</div></div>
+<div class="stepline"><div class="padder"><Stepline /></div></div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .padder {
+    padding: 0.5em;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .timeline,
+  .stepline {
+    padding: 0;
+    height: 100vh;
+    overflow-y: auto;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  .timeline {
+    text-align: center;
+    color: white;
+    background-color: blue;
   }
-  .read-the-docs {
-    color: #888;
+
+  .stepline {
+    background-color: white;
+    flex: 1;
   }
 </style>
---
+++ rebg-web/src/lib/Step.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let depth, idx, adr, asm;
+
+    // nbps
+    $: indent = "\u00A0".repeat(depth);
+    $: index = parseInt(idx).toString().padStart(4, "\u00A0");
+    $: address = "0x" + parseInt(adr).toString(16)
+</script>
+
+<div>
+    <span class="i">{indent}{index}</span>
+    <span class="adr">{address}</span>
+    <span>{asm}</span>
+</div>
+
+<style>
+    div {
+        font-family: monospace;
+    }
+    .i {
+        color: blue;
+        background-color: white;
+    }
+    .adr {
+        color: red;
+    }
+</style>
