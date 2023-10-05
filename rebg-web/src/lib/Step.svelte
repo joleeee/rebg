72d7ab01898801345669740b3b87924b258f034d
Jonas <jonas@jole.xyz>
2023-10-06 01:40:29 +0200

    add address selection and sync it

--- rebg-web/src/lib/Step.svelte
+++ rebg-web/src/lib/Step.svelte
@@ -1,14 +1,29 @@
 <script>
+    import { selectedAddress } from "./stores.js";
+
     export let depth, idx, adr, asm;
 
     $: indent = "\u00A0".repeat(depth); // nbsp
     $: index = parseInt(idx).toString().padStart(4, "\u00A0");
     $: address = "0x" + parseInt(adr).toString(16);
+
+    function click() {
+        selectedAddress.set(address);
+    }
+
+    let selected = null;
+    $: highlight = selected == address;
+
+    selectedAddress.subscribe((x) => (selected = x));
 </script>
 
-<div>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div on:click={click}>
     <span class="i">{indent}{index}</span>
-    <span class="adr">{address}</span>
+    <span class="adr" style={highlight ? "background-color: #ff000088;" : ""}
+        >{address}</span
+    >
     <span>{asm}</span>
 </div>
 
