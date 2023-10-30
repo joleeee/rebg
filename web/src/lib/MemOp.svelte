a19f6d9e38f4c85299038f724d68d9135a8823c3
Jonas <jonas@jole.xyz>
2023-10-30 20:06:48 +0100

    basic working memops ui

---
+++ web/src/lib/MemOp.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let kind = "";
+    export let adr = 0;
+    export let value = 0;
+
+    $: Address = "0x" + adr.toString(16);
+    $: Value = "0x" + value.toString(16);
+    $: Arrow = arrow(kind);
+
+    function arrow(k) {
+        if (k === "r") {
+            return "->";
+        } else if (k == "w") {
+            return "<-";
+        } else {
+            return "??";
+        }
+    }
+</script>
+
+<div>
+    <span>{Address}</span>
+    {Arrow} <span>{Value}</span>
+</div>
--- web/src/lib/MemOps.svelte
+++ web/src/lib/MemOps.svelte
@@ -1,1 +1,27 @@
-<div>helo</div>
+<script>
+    import MemOp from "./MemOp.svelte";
+    import { memOpsStore } from "./ws";
+
+    export let reads = [];
+    export let writes = [];
+
+    memOpsStore.subscribe(recv_memOps);
+
+    function recv_memOps(memOps) {
+        if (memOps === null) {
+            return;
+        }
+
+        reads = memOps.r;
+        writes = memOps.w;
+    }
+</script>
+
+<div>
+    {#each reads as r}
+        <MemOp kind="r" adr={r[0]} value={r[1]} />
+    {/each}
+    {#each writes as w}
+        <MemOp kind="w" adr={w[0]} value={w[1]} />
+    {/each}
+</div>
