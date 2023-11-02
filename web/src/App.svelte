b6597347fec02a6431749a537cf27c735f6353f4
Jonas <jonas@jole.xyz>
2023-11-02 16:29:40 +0100

    basic memory view

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,12 +1,16 @@
 <script>
   import MemOps from "./lib/MemOps.svelte";
+    import Memory from "./lib/Memory.svelte";
   import Regs from "./lib/Regs.svelte";
   import Stepline from "./lib/Stepline.svelte";
   import Straceline from "./lib/Straceline.svelte";
 </script>
 
 <div class="timeline"><div class="padder">some list here</div></div>
-<div class="stepline"><div class="padder"><Stepline /></div></div>
+<div class="middle">
+  <div class="stepline"><div class="padder"><Stepline /></div></div>
+  <div class="memory"><div class="padder"><Memory /></div></div>
+</div>
 <div class="right">
   <div class="regs"><div class="padder"><Regs /></div></div>
   <div class="memops"><div class="padder"><MemOps /></div></div>
@@ -19,7 +23,7 @@
   }
 
   .timeline,
-  .stepline,
+  .middle,
   .right {
     padding: 0;
     height: 100vh;
@@ -33,9 +37,21 @@
     background-color: blue;
   }
 
+  .middle {
+    min-width: 12em;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
   .stepline {
     background-color: white;
     min-width: 40vw;
+    overflow: auto;
+  }
+
+  .memory {
+    background-color: gainsboro;
   }
 
   .right {
---
+++ web/src/lib/Memory.svelte
@@ -0,0 +1,26 @@
+<script>
+    let data = [
+        ["0x500800", "0x4000801000a00000", "0x4000801000a00000"],
+        ["0x500810", "0x4000801000a00000", "0x4000801000a00000"],
+    ];
+
+    const height = 8;
+    const width = 2;
+</script>
+
+<div>
+    {#each data as row}
+        <div>
+            <span>{row[0]}:</span>&nbsp;
+            {#each row.slice(1) as entry}
+                &nbsp;<span>{entry}</span>
+            {/each}
+        </div>
+    {/each}
+</div>
+
+<style>
+    div {
+        font-family: monospace;
+    }
+</style>
