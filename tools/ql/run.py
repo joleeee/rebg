bdaf82d20dfa557e64cb246805f45a9870314539
Jonas <jonas@jole.xyz>
2023-12-18 20:08:12 +0100

    patch amd64 x8664 isa check

--- tools/ql/run.py
+++ tools/ql/run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from qiling import Qiling
-from qiling.const import QL_ARCH, QL_INTERCEPT
+from qiling.const import QL_ARCH, QL_INTERCEPT, QL_VERBOSE
+from qiling.extensions import pipe
 from qiling.os.mapper import QlFsMappedObject
 from typing import List
 from pprint import pprint
@@ -10,7 +11,6 @@ import os
 # from capstone import *
 from unicorn import x86_const, arm64_const
 
-# md = Cs(CS_ARCH_X86, CS_MODE_64)
 X86_REGS = [
     "rax",
     "rcx",
@@ -235,6 +235,67 @@ def sys_mprotect(ql, addr, len, prot, ret):
     syscall = f'mprotect(0x{addr:x}, 0x{len:x}, 0x{prot:x}) = 0x{ret:x}'
     ser.syscall(syscall.encode())
 
+
+ENABLE = False
+def enable_rebg(ql):
+    global ENABLE
+    if ENABLE:
+        return
+    ENABLE = True
+
+    ql.hook_code(code, begin=bin_low, end=bin_high)
+    ql.hook_mem_read(mem_read)
+    ql.hook_mem_write(mem_write)
+
+    # TODO find a nice way to do all of the syscalls
+    for name, func in [
+        ("brk", sys_brk),
+        ("exit_group", sys_exit_group),
+        ("close", sys_close),
+        ("mmap", sys_mmap),
+        ("uname", sys_uname),
+        ("mprotect", sys_mprotect),
+        ("openat", sys_openat),
+        ("read", sys_read),
+        ("write", sys_write),
+    ]:
+        ql.os.set_syscall(name, func, QL_INTERCEPT.EXIT)
+
+
+def try_patch_isa(ql: Qiling):
+    # 00023881  8b8a28030000       mov     ecx, dword [rdx+0x328]
+    # 00023887  89cf               mov     edi, ecx
+    # 00023889  4421c7             and     edi, r8d  {0x0}
+    # 0002388c  39f9               cmp     ecx, edi
+    # 0002388e  0f85000b0000       jne     0x24394  // from here
+
+    # 00023894  488d50f8           lea     rdx, [rax-0x8]
+    # 00023898  4839f0             cmp     rax, rsi
+    # 0002389b  75c3               jne     0x23860
+
+    # 0002389d  4c89ce             mov     rsi, r9  {0x3010102464c457f}  // to here
+    # 000238a0  4c89ff             mov     rdi, r15
+    # 000238a3  e8d872ffff         call    sub_1ab80
+
+    pre = bytes.fromhex("8b8a2803000089cf4421c739f9")
+    ins = bytes.fromhex("0f85000b0000")
+    skip = bytes.fromhex("488d50f84839f075c3")
+
+    def bypass_isa_check(ql: Qiling) -> None:
+        print("Bypassing ISA Check...")
+        ql.arch.regs.rip += len(ins) + len(skip)
+
+    for start, end, perm, label, img in ql.mem.get_mapinfo():
+        if label != "ld-linux-x86-64.so.2":
+            continue
+        if "x" not in perm:
+            continue
+
+        adrs = ql.mem.search(pre + ins + skip, begin=start, end=end)
+        for adr in adrs:
+            ql.hook_address(bypass_isa_check, adr + len(pre))
+
+
 def run(rootfs, argv):
     ql = Qiling(argv, rootfs)
 
@@ -250,6 +311,7 @@ def run(rootfs, argv):
     ]
 
     # simplification of reality
+    global bin_low, bin_high
     bin_low = min([start for start, _ in binary_offsets])
     bin_high = max([end for _, end in binary_offsets])
 
@@ -260,29 +322,13 @@ def run(rootfs, argv):
         file = img.encode()
         ser.libload(file, start, end)
 
-    # ql.run(end=0x400000)
-    # snap = ql.save()
-    # ql.restore(snap)
+    # ql.hook_address(lambda ql: enable_rebg(ql), 0x00007FFFB7EEA5F0)
+    # ql.os.stdin = pipe.SimpleInStream(0)
+    # ql.os.stdin.write(b"abcde\n")
 
-    ql.hook_code(code, begin=bin_low, end=bin_high)
-    ql.hook_mem_read(mem_read)
-    ql.hook_mem_write(mem_write)
-
-    # TODO find a nice way to do all of the syscalls
-    for (name, func) in [
-        ("brk", sys_brk),
-        ("exit_group", sys_exit_group),
-        ("close", sys_close),
-        ("mmap", sys_mmap),
-        ("uname", sys_uname),
-        ("mprotect", sys_mprotect),
-        ("openat", sys_openat),
-        ("read", sys_read),
-        ("write", sys_write),
-    ]:
-        ql.os.set_syscall(name, func, QL_INTERCEPT.EXIT)
-
-    ql.os.run()
+    try_patch_isa(ql)
+    enable_rebg(ql)
+    ql.run()
 
 
 if __name__ == "__main__":
